1d7167ea23b79e470f6e6f02513d29aac62cca30
rahulchakradhar30 <rahulchakradharperepogu@gmail.com>
2026-02-24 12:55:22 +0530

    Premium movie layout redesign + formatting fixes

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-black border-t border-gray-800 mt-16 py-6 text-center text-gray-400 text-sm">
       <p>© 2026 Chakradhar OTT Platform</p>
-      <p className="mt-2">Created by Rahul Chakradhar & CHAT GPT</p>
+      <p className="mt-2">Created by Rahul Chakradhar</p>
     </footer>
   );
 }
